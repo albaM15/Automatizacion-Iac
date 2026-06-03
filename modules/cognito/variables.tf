1b29dc926ae683d7af1d905818c4f496cf0b045e
alba <albamalca15@gmail.com>
2026-06-02 22:36:38 -0500

    refactor: remove deprecated Cognito and Rekognition Terraform files; add new configuration files and variables

---
+++ modules/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, ci, prod)"
+  type        = string
+}
